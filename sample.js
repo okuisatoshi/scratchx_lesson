f449c788a6f1ae37128ef5279431c8cf71af308a
Satoshi Okui <okuisatoshi@mac.com>
2017-05-24 17:32:19 +0900

    Replace sample.js

--- sample.js
+++ sample.js
@@ -7,15 +7,22 @@
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-	return {status: 2, msg: 'Ready'};
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.my_first_block = function() {
+        // Code that gets executed when the block is run
     };
 
     // Block and block menu descriptions
     var descriptor = {
-	blocks: [
-	]
+        blocks: [
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
+        ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('My first extension', descriptor, ext);
 })({});
+

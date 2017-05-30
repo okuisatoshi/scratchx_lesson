37641e9cce813908730462a187156a292ade4855
okuisatoshi <okuisatoshi@mac.com>
2017-05-30 10:50:42 +0900

    Replace the content of sample

--- sample.js
+++ sample.js
@@ -1,3 +1,5 @@
+// Sample taken from https://github.com/LLK/scratchx/wiki
+
 (function(ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
@@ -5,15 +7,21 @@
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-	return {status: 2, msg: 'Ready'};
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.my_first_block = function() {
+        // Code that gets executed when the block is run
     };
 
     // Block and block menu descriptions
     var descriptor = {
-	blocks: [
-	]
+        blocks: [
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
+        ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('My first extension', descriptor, ext);
 })({});
